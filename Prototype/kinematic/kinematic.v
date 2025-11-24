d4854ad575c4d1bc590cf25829e33c36c63add9a
Kevin Le <Kevin.Le02@student.csulb.edu>
2025-11-23 18:23:34 -0800

    Created kinematic folder. Will be module that calculates x and y coordinates

---
+++ Prototype/kinematic/kinematic.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/23/2025 05:01:40 PM
+// Design Name: 
+// Module Name: kinematic
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module kinematic(
+    input wire ax, ay,
+    output wire pixel_x, pixel_y
+
+    );
+    
+    //Declare Local Parameters
+    localparam gravity = -9.8;
+    
+    
+    
+    
+    
+    
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
