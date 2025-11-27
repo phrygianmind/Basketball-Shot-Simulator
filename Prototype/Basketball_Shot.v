386baf37a03bc49030acd428f19a7c21bc8c7485
Kevin Le <Kevin.Le02@student.csulb.edu>
2025-11-26 18:02:04 -0800

    This file will be our very top module to combine other modules

---
+++ Prototype/Basketball_Shot.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/26/2025 05:57:56 PM
+// Design Name: 
+// Module Name: Basketball_Shot
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Basketball_Shot(
+    //VGA
+    input CLK100MHZ, 
+    //input wire reset,             // optional can connect to a button
+    output wire [11:0] rgb,
+    output wire VGA_HS,
+    output wire VGA_VS
+    
+    //Accelerometer
+    
+    
+    //Seven Seg LED
+
+    );
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+endmodule
