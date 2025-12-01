c6cff151b23e110603062135e523b535b2acd757
Kevin Le <Kevin.Le02@student.csulb.edu>
2025-12-01 12:02:36 -0800

    Edited VGA Testbench to remove undefined values

--- Prototype/VGA/testbench/VGA_tb.v
+++ Prototype/VGA/testbench/VGA_tb.v
@@ -28,22 +28,33 @@ module VGA_tb(
     wire VGA_HS_tb;
     wire VGA_VS_tb;
     
-    VGA UUT (.CLK100MHZ(clk100_tb), .rgb(rgb_tb), .VGA_HS(VGA_HS_tb), .VGA_VS(VGA_VS_tb));
+    // Add registers for ball position inputs
+    reg [9:0] ball_x_tb = 320;  // Center of screen (640/2)
+    reg [9:0] ball_y_tb = 240;  // Center of screen (480/2)
     
-    //Period
+    // Connect all inputs including ball_x and ball_y
+    VGA UUT (
+        .CLK100MHZ(clk100_tb), 
+        .ball_x(ball_x_tb),     // Now connected
+        .ball_y(ball_y_tb),     // Now connected
+        .rgb(rgb_tb), 
+        .VGA_HS(VGA_HS_tb), 
+        .VGA_VS(VGA_VS_tb)
+    );
+    
+    // Clock generation (100 MHz = 10ns period)
     always #5 clk100_tb = ~clk100_tb;
     
-    
-    initial
-    begin
+    initial begin
         clk100_tb = 0;
-        #20000000;
+        
+        
+        #20000000;  // Run for 20ms
         $stop;
     end
     
-    
-    
 endmodule
+
 
 
 
